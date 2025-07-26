6989e8ae96a61a0c40c40e07caccbf90eb8dfa1a
rantoinne <raviast1719@outlook.com>
2025-07-26 13:21:21 +0530

    feat: basic boiler plate node express ws server

---
+++ server/index.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+// import session from 'express-session'
+import expressWs from 'express-ws'
+
+const { app } = expressWs(express())
+
+app.use(bodyParser.json({ limit: '10mb' }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  cors({
+    origin: (_origin, callback) => {
+      callback(null, true)
+    },
+    optionsSuccessStatus: 200,
+  }),
+)
+
+app.get('/ding', (_, res) => res.send('dong!'))
+
+const port = Number(process.env.NODE_PORT) || 8000
+const server = app.listen(port, '0.0.0.0', () => {
+  console.log(`Server up and running on port ${port}`)
+})
+
+process.on('SIGINT', async () => {
+  // handle graceful shutdown
+})
+
+export default server
